1b3d6643b0f21292e1a28f522b17a074e92b0067
Kunal Singh <kunalsingh.k@kunal-exuberant.local>
2019-07-30 02:02:05 +0530

    added models

--- src/main/java/models/Cell.java
+++ src/main/java/models/Cell.java
@@ -1,4 +1,61 @@
 package models;
 
 public class Cell {
+    private int id;
+    private String name;
+    private Type type;
+    private String icon;
+    private String description;
+
+    public Cell(){
+
+    }
+
+    public Cell(int id, String name, Type type, String icon, String description){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.icon = icon;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
---
+++ src/main/java/models/Type.java
@@ -0,0 +1,12 @@
+package models;
+
+public enum Type {
+    CITY,
+    TRANSPORT,
+    INDUSTRY,
+    TAX,
+    WORKS,
+    FREE,
+    START,
+    JAIL
+}
