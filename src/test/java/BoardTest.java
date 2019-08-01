fb554eb6f38ff596775a52cb9bfe980467d84601
Ashutosh Tripathi <ashutosh.g.tripathi@gmail.com>
2019-08-01 22:43:52 +0530

    Board cells config and extra junit asserts

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -54,8 +54,8 @@ public class BoardTest {
 
     @Test
     public void boardInitializationTest() throws IOException{
-        board = new Board();
-        board.setBoard(new GameService(objectMapper).initializeBoard());
+        Cell[] cells  = new GameService(objectMapper).initializeBoard();
+        board.setBoard(cells);
         Assert.assertEquals("Number of cells is not matching", board.getBoard().length, 40);
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[0].getName(), "Starting Cell");
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[1].getName(), "Bangalore");
@@ -63,16 +63,46 @@ public class BoardTest {
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[3].getName(), "Information Technology");
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[4].getName(), "New Delhi");
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[5].getName(), "Railways");
-        Assert.assertEquals("Number of cells is not matching", board.getBoard()[6].getName(), "Chennai");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[6].getName(), "Kolkata");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[7].getName(), "Chennai");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[8].getName(), "Agriculture");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[9].getName(), "Community card");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[10].getName(), "Jail");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[11].getName(), "Steel");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[12].getName(), "Varanasi");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[13].getName(), "Water works");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[14].getName(), "Prayagraj");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[15].getName(), "Aerodrome");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[16].getName(), "Haridwar");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[17].getName(), "Hampi");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[18].getName(), "Community card");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[19].getName(), "Film");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[20].getName(), "Parking");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[21].getName(), "Agartala");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[22].getName(), "Construction");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[23].getName(), "Textile");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[24].getName(), "Panaji");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[25].getName(), "Roadways");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[26].getName(), "Chance card");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[27].getName(), "Ooty");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[28].getName(), "Food");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[29].getName(), "Gangtok");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[30].getName(), "Go to Jail");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[31].getName(), "Jalandhar");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[32].getName(), "Income tax");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[33].getName(), "Hyderabad");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[34].getName(), "Automobile");
 
         Stream.of(board.getBoard()).forEach(cell -> System.out.println(cell.getName()+" "+cell.getId()));
 
-        Assert.assertEquals("Number of cells is not matching", board.getBoard()[35].getId(), 36);
-        Assert.assertEquals("Number of cells is not matching", board.getBoard()[35].getName(), "Hyderabad");
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[35].getId(), 35);
+        Assert.assertEquals("Number of cells is not matching", board.getBoard()[35].getName(), "Waterways");
 
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[36].getName(), "GST");
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[37].getName(), "Clean Energy");
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[38].getName(), "Ladakh");
         Assert.assertEquals("Number of cells is not matching", board.getBoard()[39].getName(), "Chance card");
     }
+
+
 }
